1250d8dcbe091cb13f32ab2b16cf11b2fdbe31da
Josef Sikula <joza.sikula@gmail.com>
2024-11-13 20:03:35 +0100

    structure xylophone draft

--- uart/lib/xylophone/xylophone.c
+++ uart/lib/xylophone/xylophone.c
@@ -2,6 +2,10 @@
 #include "gpio.h"
 #include "gpio.c"
 
+volatile uint8_t currentNote = 0;
+volatile uint8_t is_note_playing = 0;
+volatile uint8_t timer_cycle = 0;
+
 void init_xylophone()
 {
  GPIO_mode_output(&DDRD, NOTE_C1);
@@ -17,6 +21,18 @@ void init_xylophone()
 
 void play_note(uint8_t note, uint8_t velocity)
 {   
+    if (note >=0 && note < 8 && !is_note_playing)
+    {
+        write_high_note(note);  
+        currentNote = note;
+        is_note_playing = 1;
+        timer_cycle = (MAX_NOTE_ON*velocity)/MAX_VELOCITY;
+
+    }        
+}
+
+void write_high_note(uint8_t note)
+{
     switch (note)
     {
     case 0:
@@ -51,5 +67,56 @@ void play_note(uint8_t note, uint8_t velocity)
        GPIO_write_high(&PORTD, NOTE_C2);
         break;   
     }
-    
+
+}
+
+void write_low_note(uint8_t note)
+{
+    switch (note)
+    {
+    case 0:
+        GPIO_write_low(&PORTD, NOTE_C1);
+        break;
+
+    case 1:
+        GPIO_write_low(&PORTD, NOTE_D1);
+        break;
+
+    case 2:
+       GPIO_write_low(&PORTD, NOTE_E1);
+        break;
+
+    case 3:
+       GPIO_write_low(&PORTD, NOTE_F1);
+        break;
+
+    case 4:
+       GPIO_write_low(&PORTD, NOTE_G1);
+        break; 
+
+    case 5:
+       GPIO_write_low(&PORTD, NOTE_A1);
+        break;
+
+    case 6:
+       GPIO_write_low(&PORTD, NOTE_H1);
+        break;
+
+    case 7:
+       GPIO_write_low(&PORTD, NOTE_C2);
+        break;   
+    }
+
+}
+
+void timer_routine()
+{
+    if (timer_cycle != 0)
+    {
+        timer_cycle--;
+    }
+    else
+    {
+        write_low_note(currentNote);
+    }
 }
--- uart/lib/xylophone/xylophone.h
+++ uart/lib/xylophone/xylophone.h
@@ -13,10 +13,16 @@
 #define NOTE_H1 PB0
 #define NOTE_C2 PB1
 
+#define MAX_NOTE_ON 40
+#define MAX_VELOCITY 127
+
 void init_xylophone();
 
 
 void play_note(uint8_t note, uint8_t velocity);
 
+void write_high_note(uint8_t note);
+void write_low_note(uint8_t note);
+void timer_routine();
 
 #endif
